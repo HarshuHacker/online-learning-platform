cd954ecc9d4917e5eddfe5c8a10495ab4574d7c3
Harsh Prasad <harshisindian.com>
2024-01-06 17:16:56 +0530

    Added Chapters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+import Chapter from "./pages/app/chapter/Chapter"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        { index: true, element: <Hero /> },
+        {
+          path: "courses",
+          children: [
+            { index: true, element: <Courses /> },
+            { path: ":id", element: <Details /> },
+          ],
+        },
+        {
+          path: "learn/:courseId",
+          element: <Learn />,
+          children: [
+            { path: "chapter/:chapterId", element: <Chapter/> }],
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/app/learn/Learn.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import style from "./Learn.module.css";
+import coursesData from "../../../data/courses.json";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+// Task4: Import all the required elements from the react-router-dom
+
+function Learn() {
+  const { courseId } = useParams();
+  const navigate = useNavigate();
+
+  const course = coursesData.find((c) => c.id === courseId);
+  return (
+    <div className={style.courses_container}>
+      <div className={style.top_head}>
+        {/* Task4: Create Link to go back to the Courses page */}
+
+        <h2 onClick={() => navigate(-1)} className={style.back}>
+          {"<<"}
+        </h2>
+
+        {/* Task4: Title of the Course */}
+        <h1 className={style.heading}>{course.title}</h1>
+      </div>
+      <div className={style.chapter_box}>
+        <div className={style.chapters}>
+          <h1>Chapters</h1>
+          <hr />
+          <ul>
+            {/*Task4: List of Chapters must be rendered here  */}
+            {course.chapters.map((chapter) => (
+              <div className={style.chapterId} key={chapter.chapter}>
+                <Link to={`chapter/${chapter.chapter}`}>{chapter.title}</Link>
+              </div>
+            ))}
+          </ul>
+        </div>
+
+        <div className={style.courses}>
+          {/**Task5:  Chapter Details Must be rendered here */}
+          <Outlet context={{ course }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Learn;
